de32679ba4b91b4dd3609f085e44442fba2d7308
Kaycheng <kaycheng717@gmail.com>
2020-09-18 17:01:00 +0800

    Add paranoia gem for soft deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_063135) do
+ActiveRecord::Schema.define(version: 2020_09_18_082755) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_09_18_063135) do
     t.boolean "online"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_vendors_on_deleted_at"
   end
 
 end
