0ec8a1f04ed3a9785fd168dc842cab0b36a39c76
Kaycheng <kaycheng717@gmail.com>
2020-09-24 14:42:07 +0800

    Finishing category actions and views

--- app/controllers/dashboard/categories_controller.rb
+++ app/controllers/dashboard/categories_controller.rb
@@ -1,4 +1,39 @@
 class Dashboard::CategoriesController < Dashboard::BaseController
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
+    @categories = Category.order(position: :asc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to dashboard_categories_path, notice: "Category was created successfully."
+    else
+      render new_dashboard_category_path, notice: "There are some errors occurred."
+    end
+  end
+
+  def update
+    @category.update(category_params)
+    redirect_to edit_dashboard_category_path(@category), notice: "Category was updated successfully."
+  end
+
+  def destroy
+    @category.delete
+    redirect_to dashboard_categories_path, notice: "Category was deleted successfully."
+  end
+
+  private
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
