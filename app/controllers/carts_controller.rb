b72cc7d274803a156d2259a450d2b8131ab4aef9
Kaycheng <kaycheng717@gmail.com>
2020-09-28 22:32:58 +0800

    Adding cart destroy action and finish cart show page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,9 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
   def show
   end
+
+  def destroy
+    session[:cart_0429] = nil
+    redirect_to root_path, notice: "The cart is empty."
+  end
 end
