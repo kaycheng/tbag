c8937c37f06861c0326e9a94617ad1ca202d8517
Kaycheng <kaycheng717@gmail.com>
2020-10-08 14:46:08 +0800

    Adding search function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @products = Product.order(created_at: :desc).limit(4)
   end
+
+  def search
+    @products = Product.where("name LIKE ?", "%#{params[:q]}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'search', to: "welcome#search"
   resources :categories, only: [:show]
   resources :products, only: [:show]
   resource :cart, only: [:show, :destroy] do
