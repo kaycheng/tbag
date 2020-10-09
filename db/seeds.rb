e37423afce8fa5bcbb25365abfe17a2cb7f25e7f
kaycheng <kaycheng717@gmail.com>
2020-10-09 17:11:06 +0800

    Adding seed data of category and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+category_list = [
+  { name: "City" },
+  { name: "Long Trip" },
+  { name: "Mountain" },
+  { name: "Working" }
+]
+
+category_list.each do |category|
+  Category.create(name: category[:name])
+end
+
+puts "Category created!"
+
+User.create(email: "admin@tbag.com", password: "123456", role: 0)
+puts "Default admin created!"
