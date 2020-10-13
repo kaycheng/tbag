99b24033a690eda1ad4b2969be096898e34411c5
kaycheng <kaycheng717@gmail.com>
2020-10-13 17:56:04 +0800

    Adding newest products on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @products = Product.order(created_at: :desc).limit(8)
+    @featured_products = Product.order(id: :asc).limit(4)
+    @newest_products = Product.order(created_at: :desc).limit(4)
     @welcome_categories = Category.order(id: :asc).limit(3)
   end
 
