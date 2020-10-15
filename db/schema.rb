189f9636f8c021e60d16d792f47743fc588651a1
kay <kaycheng717@gmail.com>
2020-10-15 23:32:25 +0800

    Update ruby version to 2.6.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_090432) do
+ActiveRecord::Schema.define(version: 2020_10_15_142936) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
