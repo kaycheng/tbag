cc4258b0560adc1acd7b8dc5b2e538b22af91c51
kaycheng <kaycheng717@gmail.com>
2020-10-17 15:00:43 +0800

    Adding smtp setting on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "goshopping_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mailgun.org',
+    port:                 587,
+    domain:               ENV['YOUR_DOMAIN_NAME'],
+    user_name:            ENV['MAILGUN_USERNAME'],
+    password:             ENV['MAILGUN_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
