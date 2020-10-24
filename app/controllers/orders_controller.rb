840c0ce93364301d8d159804b6f70d274e2a761c
kaycheng <kaycheng717@gmail.com>
2020-10-24 15:37:40 +0800

    Fixed the website routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,7 @@ class OrdersController < ApplicationController
         productName: "TBagcem",
         amount: current_cart.total_price.to_i,
         currency: "TWD",
-        confirmUrl: "https://tbagcem.herokuapp.com/orders/confirm",
+        confirmUrl: "http://www.tbagcem.com/orders/confirm",
         orderId: @order.num
       })
 
@@ -75,7 +75,7 @@ class OrdersController < ApplicationController
       productName: "BFF",
       amount: @order.total_price.to_i,
       currency: "TWD",
-      confirmUrl: "https://tbagcem.herokuapp.com/orders/#{@order.id}/pay_confirm",
+      confirmUrl: "http://www.tbagcem.com/orders/#{@order.id}/pay_confirm",
       orderId: @order.num
     })
 
